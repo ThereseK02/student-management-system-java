851e2f993f1857ed5a1816193b78822ab772e6d9
Therese Kabayanja <251678928+ThereseK02@users.noreply.github.com>
2026-05-12 23:07:55 -0500

    Add author information to Authenticatable interface

--- src/Authenticatable.java
+++ src/Authenticatable.java
@@ -2,6 +2,7 @@
  * Defines the Authenticatable interface for the Student Management System.
  * This interface establishes the authentication contract for users who are authorized to access the system. 
  * The abstract User class implements this interface to provide login functionality across the application.
+ *
  * Author: Therese Kabayanja
  */
 
